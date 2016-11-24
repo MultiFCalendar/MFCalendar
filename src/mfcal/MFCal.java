083fbf1bc3ac50bccdc713e8d5585eb8a3d2d988
Ahmet Emin Kaplan <akaplan@gtu.edu.tr>
2016-11-24 13:38:05 +0200

    Update MFCal.java
    Delete from files metodu eklendi

--- src/mfcal/MFCal.java
+++ src/mfcal/MFCal.java
@@ -42,5 +42,36 @@ public class MFCal {
 
         return str;
     }
+    
+    public static void deleteFromFiles(String fileName, ArrayList<String> deleteItem) throws FileNotFoundException, IOException {
+        FileReader fileReader = new FileReader(new File(fileName));
+        BufferedReader br = new BufferedReader(fileReader);
+        ArrayList<String> newFil = new ArrayList<String>();
+
+        String line = null;
+        br.readLine();
+        int c =0;
+
+        while ((line = br.readLine()) != null) {
+            if (!deleteItem.contains(line)) {
+                newFil.add(c, line);
+                c++;
+            }
+        }
+        br.close();
+
+         
+        FileWriter fw = new FileWriter(new File(fileName));
+        BufferedWriter bw = new BufferedWriter(fw);
+        String str = MFCal.ActiveOrPassive(fileName);
+        bw.write(str);
+        
+        for(int i = 0 ; i < newFil.size(); ++i){
+            bw.append(newFil.get(i));
+            bw.append("\r\n");
+        }
+        bw.close();// be sure to close BufferedWriter
+
+    }
 
 }
