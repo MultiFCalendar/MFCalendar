b912f0f3b2b699804d35438ace6cc7ba4d2b8282
meulusoy <ercanu1995@gmail.com>
2016-12-17 00:11:57 +0200

    Update ActiveJobThread.java

--- src/mfcal/ActiveJobThread.java
+++ src/mfcal/ActiveJobThread.java
@@ -1,1 +1,97 @@
-// Will be implemented
+package mfcal;
+
+
+
+import java.io.*;
+
+import java.util.logging.Level;
+
+import java.util.logging.Logger;
+
+
+
+
+
+/**
+
+ *  Bu sinif, uygulama acildigi zaman
+
+ *  yapilmasi gereken bir aktif gorev var ise, 
+
+ *  bu aktif gorev icin bir FtpClient objesi alir.
+
+ *  Arka planda bir thread kullanarak
+
+ *  bu aktif gorevi yapar.
+
+ * @author mutlu koktemir
+
+ */
+
+public class ActiveJobThread implements Runnable{
+
+    
+
+    private Thread thread;
+
+    FtpClient ftpClient;
+
+    
+
+    /**
+
+     * 
+
+     * @param ftpClientOther aktif gorevi yapmak icin FtpClient objesi alir.
+
+     * Thread'i baslatir.
+
+     */
+
+    public ActiveJobThread(FtpClient ftpClientOther)
+
+    {
+
+        ftpClient = ftpClientOther;
+
+        thread = new Thread(this,"other");
+
+        thread.start();
+
+        
+
+    }
+
+    
+
+    
+
+    @Override
+
+    public void run() 
+
+    {
+
+        
+
+        try {
+
+        	
+
+            ftpClient.sendFile();
+
+            
+
+        } catch ( IOException ex ) {
+
+            Logger.getLogger(ActiveJobThread.class.getName()).log(Level.SEVERE, null, ex);
+
+        }
+
+        
+
+        
+
+    }
+
+}
